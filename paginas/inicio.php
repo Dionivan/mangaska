2f15ced9004eef33735529772e0b519c882c9e1c
unknown <santosdionivan@gmail.com>
2022-06-13 00:12:46 -0300

    inicio

---
+++ paginas/inicio.php
@@ -0,0 +1,57 @@
+<?php require_once "../src/view/header.php" ?>
+<?php require_once "../src/view/header_nav.php" ?>
+    <main class="Corpo">
+        <div class="nav-main">
+            <div class="main-header">Lançamentos</div>
+            <button type="button" class="btn btn-primary btn-sm col-lg6">Todos</button>
+            <button type="button" class="btn btn-secondary btn-sm">Minhas Leituras</button>
+        </div>
+        <div class="container">
+            <a href="../mangas/swordmaster.php">
+                <div class="lançamento">
+                    <img src="https://static3.mangalivre.net/capas/C_NaH9eLuMBYvgoFKeT0sQ/15158/626d55a00fb5e_external_cover.jpg" height="220px" width="150px" alt="manga-1">
+                    <span>
+                        Jin Runkandel 
+
+                            O filho mais novo de Runkandel, o melhor clã de esgrima do continente 
+
+                            e o pior idiota de toda a história de Runkandel.
+
+                            Como ele é miseravelmente expulso de seu clã e encontra sua morte lamentável, outra chance é dada a ele.
+
+                            'Como você quer usar esse poder?' 
+                    </span>
+                </div>
+            </a>
+            <a href="../mangas/mushoku-tensei.php">
+                <div class="lançamento">
+                    <img src="https://static3.mangalivre.net/capas/vcHnmg3zmSAeq9xI3cibnw/2551/618af7773b22bexternal_cover.jpg" height="220px" width="150px" alt="manga-2">
+                    <span>
+                        Um NEET de 34 anos é expulso de casa pelos seus irmãos. 
+                    Quando se via sem dinheiro ou lugar pra dormir, ele lembra que sua vida poderia ter sido muito melhor se ele tivesse se esforçado.
+                    Então vendo um caminhão vindo em alta velocidade prestes a atropelar 3 estudantes,
+                    ele tenta salva-los e acaba morrendo atropelado. Quando ele decide abrir os olhos mais uma vez,
+                    ele percebe que renasceu em um mundo de Espadas e Magia como Rudeus Greyrat. Nascido em um novo mundo,
+                    numa nova vida, Rudeus decide: "Dessa vez, eu vou viver minha vida ao máximo pra não ter nenhum arrependimento!".
+                        Assim começa a jornada de um homem recomeçando a sua vida.
+                    </span>
+                </div>
+            </a>
+            <a href="">
+                <div class="lançamento">
+                    <img src="https://static3.mangalivre.net/capas/mpBdBkqe2fN-P5yfzuSIxQ/13766/61625a016a9b7_capa.jpg"  height="220px" width="150px" alt="manga-3">
+                    <span>
+                        Um grupo de batedores foi designado para a área da seita do Monte Hua por seu grande líder, ‘O Louva-a-Deus’.
+                        O protagonista ‘Seol Hwi’ encara o Mestre Hua Sect ‘Goo Jong
+                        Myung, 'e cai em perigo mortal.
+                        Uma janela de status então aparece e o faz retornar no tempo antes de aceitar a missão de reconhecimento.
+                        Depois de receber a mesma missão de reconhecimento que o deixou morto, Seol Hui luta para mudar a realidade inevitável.
+                        Uma janela de status que aparece a cada crise.
+                        Escolhas com futuro incerto.
+                        A história de Seol Hwi enquanto ele tenta encontrar a melhor decisão em seu mundo misturada com vida e morte.
+                    </span>
+                </div>
+            </a>
+        </div>      
+    </main>
+<?php require_once "../src/view/footer.php"?>
